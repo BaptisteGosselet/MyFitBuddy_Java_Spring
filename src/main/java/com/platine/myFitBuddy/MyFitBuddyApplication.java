ad373841f813399e8c8cfedc910ccb36eaa86289
Baptiste Gosselet <baptiste.gosselet@outlook.fr>
2024-10-10 19:12:31 +0200

    :tada: - spring init

---
+++ src/main/java/com/platine/myFitBuddy/MyFitBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.platine.myFitBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitBuddyApplication.class, args);
+	}
+
+}
