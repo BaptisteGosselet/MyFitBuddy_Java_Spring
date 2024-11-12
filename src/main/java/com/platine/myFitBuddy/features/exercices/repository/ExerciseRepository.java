1863af92aea5ecb2837cd34e47104e0d77c2d5c8
Baptiste Gosselet <baptiste.gosselet.etu@univ-lille.fr>
2024-11-12 17:04:50 +0100

    :sparkles: - Trad exercises

--- src/main/java/com/platine/myFitBuddy/features/exercices/repository/ExerciseRepository.java
+++ src/main/java/com/platine/myFitBuddy/features/exercices/repository/ExerciseRepository.java
@@ -1,5 +1,7 @@
 package com.platine.myFitBuddy.features.exercices.repository;
 
+import com.platine.myFitBuddy.features.exercices.model.Exercise;
+import com.platine.myFitBuddy.features.exercices.model.MuscleGroup;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,14 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.platine.myFitBuddy.features.exercices.model.Exercise;
-import com.platine.myFitBuddy.features.exercices.model.MuscleGroup;
-
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
   @Query(
     "SELECT e FROM Exercise e WHERE " +
-    "(:key IS NULL OR REPLACE(LOWER(e.key), ' ', '_') LIKE '%' || CAST(:key AS text) || '%') AND " +
+    "(:key IS NULL OR " +
+    "LOWER(e.key) LIKE '%' || LOWER(CAST(:key AS text)) || '%' OR " +
+    "LOWER(e.labelEn) LIKE '%' || LOWER(CAST(:key AS text)) || '%' OR " +
+    "LOWER(e.labelFr) LIKE '%' || LOWER(CAST(:key AS text)) || '%') AND " +
     "(:muscleGroup IS NULL OR e.muscleGroup = :muscleGroup)"
   )
   Page<Exercise> findByKeyAndMuscleGroup(
