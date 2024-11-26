df93e0e09d577db5f35fa874b02d0daa0f8dcf04
Kyllian Pinceel <kyllian.pinceel.etu@univ-lille.fr>
2024-11-26 17:36:19 +0100

    :bug: put NoArgConstructor

--- src/main/java/com/platine/myFitBuddy/features/sessions/model/SessionCreateForm.java
+++ src/main/java/com/platine/myFitBuddy/features/sessions/model/SessionCreateForm.java
@@ -2,11 +2,13 @@ package com.platine.myFitBuddy.features.sessions.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SessionCreateForm {
   private String name;
 }
