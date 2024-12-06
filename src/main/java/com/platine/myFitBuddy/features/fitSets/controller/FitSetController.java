13a662cd6e0f138a2f7eae613ccf3e91d51429de
Baptiste Gosselet <baptiste.gosselet@outlook.fr>
2024-12-06 11:20:02 +0100

    :bug: - requestbody import

--- src/main/java/com/platine/myFitBuddy/features/fitSets/controller/FitSetController.java
+++ src/main/java/com/platine/myFitBuddy/features/fitSets/controller/FitSetController.java
@@ -3,7 +3,6 @@ package com.platine.myFitBuddy.features.fitSets.controller;
 import com.platine.myFitBuddy.features.fitSets.model.FitSet;
 import com.platine.myFitBuddy.features.fitSets.model.FitSetCreateForm;
 import com.platine.myFitBuddy.features.fitSets.model.FitSetUpdateForm;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/sets")
