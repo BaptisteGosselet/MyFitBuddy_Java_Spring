5eb735f8cd4dc3d56058531641b38d142935ae23
Baptiste Gosselet <baptiste.gosselet@outlook.fr>
2024-12-10 09:42:24 +0100

    :sparkles: - feeling note record

--- src/main/java/com/platine/myFitBuddy/features/fitRecords/controller/FitRecordController.java
+++ src/main/java/com/platine/myFitBuddy/features/fitRecords/controller/FitRecordController.java
@@ -1,14 +1,14 @@
 package com.platine.myFitBuddy.features.fitRecords.controller;
 
 import com.platine.myFitBuddy.features.fitRecords.model.FitRecord;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import com.platine.myFitBuddy.features.fitRecords.model.FitRecordNoteForm;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/records")
@@ -22,10 +22,10 @@ public interface FitRecordController {
   @PostMapping(value = "/create/{sessionId}", produces = "application/json")
   ResponseEntity<FitRecord> createRecord(@PathVariable("sessionId") final long sessionId);
 
-  @PostMapping(value = "/note/{sessionId}", produces = "application/json")
+  @PostMapping(value = "/note/{recordId}", produces = "application/json")
   ResponseEntity<FitRecord> setFeelingNote(
-    @PathVariable("sessionId") final long sessionId,
-    @RequestBody final String note
+    @PathVariable("recordId") final long recordId,
+    @RequestBody final FitRecordNoteForm form
   );
 
   @DeleteMapping(value = "/delete/{recordId}", produces = "application/json")
